71286c1632a99b76049cc0aac568a4968b937359
XINICOM <XINICOM@outlook.com>
2026-09-13 23:04:00 +0800

    FIX: compilation mode keybinding and using MSYS2 bash

--- init.el
+++ init.el
@@ -3,6 +3,20 @@
 (add-to-list 'load-path
 	     (expand-file-name (concat user-emacs-directory "lisp")))
 
+;; ;; 让 Compilation mode 使用 MSYS2 的 Bash
+;; (setenv "MSYSTEM" "MINGW64")
+;; (setq shell-file-name "D:/msys64/usr/bin/bash.exe")
+;; (setq shell-command-switch "-lc")
+
+(setq shell-file-name "D:/msys64/usr/bin/bash.exe")
+(setq shell-command-switch "-c")
+(setenv "PATH"
+        (concat "D:/msys64/mingw64/bin;D:/msys64/usr/bin;"
+                (getenv "PATH")))
+(setenv "MSYSTEM" "MINGW64")
+(setq compile-command "")
+
+
 (require 'init-const)
 
 (require 'init-startup)
--- lisp/init-keymap.el
+++ lisp/init-keymap.el
@@ -2,5 +2,7 @@
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+(global-set-key (kbd "M-c") 'compile)
+
 
 (provide 'init-keymap)
